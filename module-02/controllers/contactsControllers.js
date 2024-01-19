315ae008e350668b351131b4436116bd143da61e
Korotenko Alex <55750546+ToyEye@users.noreply.github.com>
2024-01-19 11:46:36 +0200

    done

--- module-02/controllers/contactsControllers.js
+++ module-02/controllers/contactsControllers.js
@@ -54,12 +54,14 @@ export const createContact = async (req, res, next) => {
 };
 
 export const updateContact = async (req, res, next) => {
+  if (Object.values(req.body).length === 0) {
+    res.status(400).json({ message: "Body must have at least one field" });
+  }
   try {
     const updatedContact = await contactsService.updateContact(
       req.params.id,
       req.body
     );
-
     res.status(200).json(updatedContact);
   } catch (error) {
     next(error);
